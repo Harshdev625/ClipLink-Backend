9d3d230f4c4d69a7856f693682bed5e216159a13
Harsh Dev <devh9933@gmail.com>
2025-04-09 16:14:33 +0530

    Refactor controllers: standardize quotes and improve error handling; add request-ip dependency

--- controllers/link.controller.js
+++ controllers/link.controller.js
@@ -1,26 +1,28 @@
-const ShortUrl = require('../models/ShortUrl');
-const { nanoid } = require('nanoid');
+const ShortUrl = require("../models/ShortUrl");
+const { nanoid } = require("nanoid");
 
 exports.createShortUrl = async (req, res) => {
-  const { url, alias, expiresAt } = req.body; // <-- Match frontend keys
+  const { url, alias, expiresAt } = req.body;
 
   const shortCode = alias || nanoid(6);
 
   const exists = await ShortUrl.findOne({ shortCode });
-  if (exists) return res.status(400).json({ message: 'Alias already taken' });
+  if (exists) return res.status(400).json({ message: "Alias already taken" });
 
   const newLink = await ShortUrl.create({
     user: req.userId,
-    originalUrl: url,           
+    originalUrl: url,
     shortCode,
-    customAlias: alias,         
+    customAlias: alias,
     expiresAt: expiresAt ? new Date(expiresAt) : null,
   });
 
-  res.json({ shortUrl: `${process.env.BASE_URL}/${shortCode}` });
+  res.json({ shortUrl: `${process.env.CLIENT_URL}/${shortCode}` });
 };
 
 exports.getUserLinks = async (req, res) => {
-  const links = await ShortUrl.find({ user: req.userId }).sort({ createdAt: -1 });
+  const links = await ShortUrl.find({ user: req.userId }).sort({
+    createdAt: -1,
+  });
   res.json(links);
 };
--- controllers/redirect.controller.js
+++ controllers/redirect.controller.js
@@ -1,24 +1,26 @@
-const ShortUrl = require('../models/ShortUrl');
-const Click = require('../models/Click');
-const uaParser = require('ua-parser-js');
+const ShortUrl = require("../models/ShortUrl");
+const Click = require("../models/Click");
+const uaParser = require("ua-parser-js");
+const requestIp = require("request-ip");
 
 exports.handleRedirect = async (req, res) => {
   const { code } = req.params;
   const shortUrl = await ShortUrl.findOne({ shortCode: code });
-  if (!shortUrl) return res.status(404).send('Not found');
+
+  if (!shortUrl) return res.status(404).send("Not found");
 
   shortUrl.clicks++;
   await shortUrl.save();
 
-  const ua = uaParser(req.headers['user-agent']);
+  const ua = uaParser(req.headers["user-agent"]);
+  const ip = requestIp.getClientIp(req);
 
-  // Log analytics
-  Click.create({
+  await Click.create({
     shortCode: code,
-    ip: req.ip,
-    device: ua.device.type || 'desktop',
-    browser: ua.browser.name,
-    os: ua.os.name,
+    ip,
+    device: ua.device.type || "desktop",
+    browser: ua.browser.name || "Unknown",
+    os: ua.os.name || "Unknown",
   });
 
   res.redirect(shortUrl.originalUrl);
